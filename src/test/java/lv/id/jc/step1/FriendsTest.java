a4bc270e25a1648b0571831060742e0dca18c33d
Jegors Cemisovs <jcemisovs@evolution.com>
2025-02-02 14:47:40 +0200

    Add unit test for validating John's attributes and friends
    This commit introduces a test case in `FriendsTest` to verify the attributes and relationships of the `John` object. It ensures the name, birthday, number of friends, and the presence of Mary as a friend are correctly implemented.

---
+++ src/test/java/lv/id/jc/step1/FriendsTest.java
@@ -0,0 +1,37 @@
+package lv.id.jc.step1;
+
+import org.assertj.core.api.InstanceOfAssertFactories;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+class FriendsTest {
+
+    /*
+        We need to test that:
+        - the name is John
+        - the date of birth is 3rd May 1990
+        - the John has two friends
+        - Mary among the friends of John
+     */
+    @Test
+    void getJohn() {
+        // Arrange
+        var friends = new Friends();
+
+        // Act
+        var john = friends.getJohn();
+
+        // Assert
+        assertThat(john)
+                .hasFieldOrPropertyWithValue("name", "John")
+                .hasFieldOrPropertyWithValue("birthday", LocalDate.parse("1990-03-05"))
+                .extracting(Person::friends)
+                .asInstanceOf(InstanceOfAssertFactories.SET)
+                .hasSize(2)
+                .extracting("name")
+                .contains("Mary");
+    }
+}
